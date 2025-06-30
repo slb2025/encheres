53833d0810d59f7289b82ae55b0f3748f8f29241
EricG44 <eric.guillouet@gmail.com>
2025-06-30 15:58:06 +0200

    Structure Java

---
+++ src/main/java/fr/eni/controller/IndexController.java
@@ -0,0 +1,18 @@
+package fr.eni.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+
+    public String AfficherAccueil() {
+        System.out.println("Appel de la méthode index");
+
+        return "index2";
+
+    }
+
+
+}
