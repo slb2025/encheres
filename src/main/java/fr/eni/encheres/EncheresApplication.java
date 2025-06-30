4151d5783e88c3360a21a82153e3c7156f114006
lbourcier2024 <bourcier.lukas44@gmail.com>
2025-06-30 14:34:56 +0200

    Change indentation

--- src/main/java/fr/eni/encheres/EncheresApplication.java
+++ src/main/java/fr/eni/encheres/EncheresApplication.java
@@ -6,8 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EncheresApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EncheresApplication.class, args);
-	}
+	public static void main(String[] args) {SpringApplication.run(EncheresApplication.class, args);}
 		//test
 }
