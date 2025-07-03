8937fd65f8a42bcc854ed0e2a0c3f8b060e1199b
lbourcier2024 <bourcier.lukas44@gmail.com>
2025-07-03 11:28:17 +0200

    ajoute une sécurité pour l'accès au page sans connexion & ajoute cookie pour une durée de vie de 7 jours

--- src/main/java/fr/eni/controller/UtilisateurController.java
+++ src/main/java/fr/eni/controller/UtilisateurController.java
@@ -2,11 +2,15 @@ package fr.eni.controller;
 
 import fr.eni.bll.UtilisateurService;
 import fr.eni.bo.Enchere;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import jakarta.servlet.http.HttpSession;
+
 
 @Controller
 public class UtilisateurController {
@@ -31,23 +35,34 @@ public class UtilisateurController {
     }
 
     @GetMapping("/PagesListeEncheresConnecte")
-    public String afficherPagesListeEncheresConnecte() {
+    public String afficherPagesListeEncheresConnecte(HttpSession session) {
+        if (session.getAttribute("pseudo") == null) {
+            return "redirect:/PageConnexion";
+        }
         return "PagesListeEncheresConnecte";
     }
 
     @PostMapping("/login")
-    public String login(
-            @RequestParam String pseudo,
-            @RequestParam String password,
-            Model model) {
-
+    public String login(@RequestParam String pseudo,
+                        @RequestParam String password,
+                        @RequestParam(required = false) String souvenir,
+                        HttpSession session,
+                        HttpServletResponse response, // POUR AJOUTER UN COOKIE
+                        Model model) {
         boolean success = utilisateurService.login(pseudo, password);
 
         if (success) {
+            if ("on".equals(souvenir)) {
+                Cookie cookie = new Cookie("pseudo", pseudo);
+                cookie.setMaxAge(7 * 24 * 60 * 60); // 7 jours
+                response.addCookie(cookie);
+            }
+            session.setAttribute("pseudo", pseudo);
             return "redirect:/PagesListeEncheresConnecte";
         } else {
             model.addAttribute("error", "Identifiant ou mot de passe incorrect.");
             return "PageConnexion";
         }
     }
+
 }
