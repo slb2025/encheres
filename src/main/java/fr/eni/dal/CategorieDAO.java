bc636ba9de9fa3ea8f43c61b4f8be38812dd44f3
EricG44 <eric.guillouet@gmail.com>
2025-07-07 09:23:59 +0200

    V1.12 : venteArticle + page Enchere nonCommencée

---
+++ src/main/java/fr/eni/dal/CategorieDAO.java
@@ -0,0 +1,14 @@
+package fr.eni.dal;
+
+import fr.eni.bo.Categorie;
+
+import java.util.List;
+
+public interface CategorieDAO {
+
+    List<Categorie> getCategories();
+
+
+}
+
+
--- src/main/java/fr/eni/dal/EnchereDAO.java
+++ src/main/java/fr/eni/dal/EnchereDAO.java
@@ -8,5 +8,4 @@ import java.util.List;
 public interface EnchereDAO {
 
     void create(ArticleVendu articleVendu);
-    List<Categorie>findCategories();
 }
